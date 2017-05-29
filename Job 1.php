1e6d0df13fbe3df7b7389f78447c0c4fa9cab71d
Rizka Marta Kartikasari <Rizkamarta05@gmail.com>
2017-05-29 12:37:19 +0700

    Add files via upload

---
+++ Job 1.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<form action="welcomepost.php" method="get">
+<pre>
+Enter your Name  : <input type="text" name="name" />
+
+Enter your Age   : <input tytpe="text" name="age" />
+
+</pre>
+		<input type="submit" value="Submit">
+		<input type="reset" value="Reset">
+
+</form>
+</body>
+</html>
---
+++ Link2a.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<?php
+$kampusku="STEKOM";
+$alamat1="Jl. Majapahit 304 Semarang";
+$alamat2="Jl. Majapahit 605 Semarang";
+echo"<a href=\"Link2b.php?kampusku=$kampusku&
+alamatl=$alamatl&alamat2\">Kampusku </a>";
+?>
+</body>
+</html>
